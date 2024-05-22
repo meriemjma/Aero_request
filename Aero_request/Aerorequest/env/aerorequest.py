b2bccda440d62a7389e5f0ccecf6b2185500c021
meriemjma <130979570+meriemjma@users.noreply.github.com>
2024-05-22 11:40:42 +0200

    Update aerorequest.py

--- Aero_request/Aerorequest/env/aerorequest.py
+++ Aero_request/Aerorequest/env/aerorequest.py
@@ -2,7 +2,6 @@ import os
 import sys
 from math import sin, cos, pi, sqrt
 from random import randrange
-
 import gym
 import numpy as np
 import pygame
@@ -26,7 +25,7 @@ class Aerorequest(gym.Env):
 
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 24}
 
-    def __init__(self, render_mode='human', obs_type='pixels'):
+    def __init__(self, render_mode=None, obs_type=None):
         """
         Initialize the Aerorequest environment.
 
@@ -41,7 +40,7 @@ class Aerorequest(gym.Env):
         # Set observation type attribute
         self.obs_type = obs_type
 
-        # Game constants
+
         self.FPS = self.metadata["render_fps"]
         self.WIDTH = 650
         self.HEIGHT = 650
@@ -53,10 +52,8 @@ class Aerorequest(gym.Env):
 
         # Initialize Pygame
         pygame.init()
-        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-        pygame.display.set_caption("Aerorequest Environment")
+        self.screen = None
         self.FramePerSec = pygame.time.Clock()
-        self.FramePerSec.tick(24)
 
         # Initialize player and target sprites
         self.load_sprites()
@@ -71,12 +68,24 @@ class Aerorequest(gym.Env):
         # Define action space
         self.action_space = spaces.Discrete(4)
 
+        # Initialize flashing variables
+        self.flash_duration = 0.5  # Duration of the flashing effect in seconds
+        self.flash_timer = 0  # Timer for controlling the flashing effect
+        self.flash_color = (255, 0, 0)  # Red color for flashing
+
+        self.cloud1_pos = [150, 200]
+        self.cloud2_pos = [400, 500]
+        self.cloud1_speed = 0.3
+        self.cloud2_speed = -0.2
+        self.sun_pos = [self.WIDTH-150, -100]
+
+
         # Define observation space based on observation type
         if obs_type == 'pixels':
             self.observation_space = spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8)
         elif obs_type == 'features':
             self.observation_space = spaces.Dict({
-                "player_position": spaces.Box(low=-100000, high=100000, shape=(2,), dtype=np.float32),#to fix
+                "player_position": spaces.Box(low=0, high=self.HEIGHT, shape=(2,), dtype=np.float32),#to fix
                 "player_angle": spaces.Box(low=0, high=360, shape=(1,), dtype=np.float32),
                 "target_position": spaces.Box(low=0, high=self.WIDTH, shape=(2,), dtype=np.float32),
                 "score": spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32)
@@ -89,24 +98,36 @@ class Aerorequest(gym.Env):
 
         # Call reset to initialize the environment
         self.reset()
-
+        
     def load_sprites(self):
         """
         Load player and target sprites.
         """
         # Loading player sprite
         self.player_width = 80
-        player_image = pygame.image.load(os.path.join("C:\\Users\\Maryem\\Desktop\\Aero_request\\Aerorequest\\env\\ressources\\drone1.png"))
+        player_image = pygame.image.load(os.path.join("Aero_request\\Aerorequest\\env\\ressources\\drone1.png"))
         self.player_image = pygame.transform.scale(player_image, (self.player_width, int(self.player_width * 0.30)))
 
         # Loading target sprite
         self.target_width = 50
         self.target_height = 50
-        target_image = pygame.image.load(os.path.join("C:\\Users\\Maryem\\Desktop\\Aero_request\\Aerorequest\\env\\ressources\\target1.png"))
+        target_image = pygame.image.load(os.path.join("Aero_request\\Aerorequest\\env\\ressources\\target1.png"))
         self.target_image = pygame.transform.scale(target_image, (self.target_width, self.target_height))
-
+        
         self.targets = [] # To store positions of target
+
         self.spawn_target()
+        # Loading sun sprite
+        sun_image = pygame.image.load(os.path.join("Aero_request\\Aerorequest\\env\\ressources\\sun.png"))
+        self.sun_image = pygame.transform.scale(sun_image, (300, 300))  
+
+        # Loading cloud sprites
+        cloud1_image = pygame.image.load(os.path.join("Aero_request\\Aerorequest\\env\\ressources\\cloud1.png"))
+        cloud2_image = pygame.image.load(os.path.join("Aero_request\\Aerorequest\\env\\ressources\\cloud2.png"))
+        self.cloud_images = [
+            pygame.transform.scale(cloud1_image, (200, 100)),  
+            pygame.transform.scale(cloud2_image, (150, 75))    
+        ]
 
 
     def spawn_target(self):
@@ -197,6 +218,14 @@ class Aerorequest(gym.Env):
             elif dist > 1000:
                 self.player.dead = True
                 self.player.respawn_timer = self.respawn_timer_max
+
+            # Check if player touches the bounds of the screen
+            if (self.player.x_position < 0 or self.player.x_position > self.WIDTH or
+                    self.player.y_position < 0 or self.player.y_position > self.HEIGHT):
+                self.player.dead = True
+                self.player.respawn_timer = self.respawn_timer_max
+                # Flash the edges red
+                self.flash_edges()                 
         else:
             # Decrement respawn timer and respawn if timer reaches 0
             self.player.respawn_timer -= 1 / self.FPS
@@ -221,72 +250,183 @@ class Aerorequest(gym.Env):
         observation = self._get_obs()
         info = self._get_info()
 
-        return observation, reward, done, False, info
+        if self.render_mode == 'human':
+            self._render_frame_human()
 
-    def render(self, mode='human'):
+        return observation, reward, done, False ,info
+
+    def flash_edges(self):
+        """
+        Flash the edges of the screen red.
+        """
+        # Set the flashing timer
+        self.flash_timer = self.flash_duration
+
+    def render(self):
         """
         Render the environment.
 
-        Args:
-            mode (str): Rendering mode, either 'human' or 'rgb_array'.
+        Returns:
+            object or np.array: Rendered image or array (only for 'rgb_array' mode).
+        """
+        if self.render_mode == 'rgb_array':
+            return self._render_frame()
+        elif self.render_mode == 'human':
+            self._render_frame_human()
+
+
+    def _render_frame(self):
+        """
+        Render the environment in RGB array mode.
 
         Returns:
-            (object or np.array): Rendered image or array.
+            np.array: RGB array of the current frame.
         """
-        if mode == 'human':
-            # Display environment in a Pygame window
-            self._render_human()
-        elif mode == 'rgb_array':
-            # Render environment as an RGB array
-            return self._render_rgb_array()
+        # Create a new canvas with the same size as the screen
+        canvas = pygame.Surface((self.WIDTH, self.HEIGHT))
+        canvas.fill((131, 176, 181))  # Fill the canvas with a background color
+        
+        # Render clouds
+        self.cloud1_pos[0] += self.cloud1_speed
+        if self.cloud1_pos[0] > self.WIDTH:
+            self.cloud1_pos[0] = -self.cloud_images[0].get_width()  
+        canvas.blit(self.cloud_images[0], self.cloud1_pos)  
 
-    def _render_human(self):
-        """
-        Render the environment in human mode.
-        """
-        # Display background
-        self.screen.fill((131, 176, 181))
+        self.cloud2_pos[0] += self.cloud2_speed
+        if self.cloud2_pos[0] < -self.cloud_images[1].get_width():  
+            self.cloud2_pos[0] = self.WIDTH
+        canvas.blit(self.cloud_images[1], self.cloud2_pos) 
+
+        # Render sun
+        canvas.blit(self.sun_image, self.sun_pos)
 
         # Render player
         player_copy = pygame.transform.rotate(self.player_image, self.player.angle)
-        self.screen.blit(
-            player_copy,
-            (
-                self.player.x_position - int(player_copy.get_width() / 2),
-                self.player.y_position - int(player_copy.get_height() / 2),
-            ),
+        player_pos = (
+            int(self.player.x_position - player_copy.get_width() / 2),
+            int(self.player.y_position - player_copy.get_height() / 2)
         )
+        canvas.blit(player_copy, player_pos)
 
         # Render target
         target_pos = (
             self.targets[0][0] - int(self.target_image.get_width() / 2),
             self.targets[0][1] - int(self.target_image.get_height() / 2)
-        )  # Calculate the position to draw the target sprite
-        self.screen.blit(self.target_image, target_pos)
+        )
+        canvas.blit(self.target_image, target_pos)
 
         # Render time and score on the left side
         font = pygame.font.SysFont(None, 36)
         time_text = font.render(f"Time: {int(self.time)}", True, (255, 255, 255))
-        score_text = font.render(f"Score: {self.score}", True, (255, 255, 255))  
+        score_text = font.render(f"Score: {self.score}", True, (255, 255, 255))
+        canvas.blit(time_text, (20, 20))
+        canvas.blit(score_text, (20, 60))
+
+        # Check if flashing effect is active
+        if self.flash_timer > 0:
+            # Render the edges with flashing red color
+            pygame.draw.rect(canvas, self.flash_color, (0, 0, self.WIDTH, 10))  # Top edge
+            pygame.draw.rect(canvas, self.flash_color, (0, 0, 10, self.HEIGHT))  # Left edge
+            pygame.draw.rect(canvas, self.flash_color, (0, self.HEIGHT - 10, self.WIDTH, 10))  # Bottom edge
+            pygame.draw.rect(canvas, self.flash_color, (self.WIDTH - 10, 0, 10, self.HEIGHT))  # Right edge
+            # Decrement the flashing timer
+            self.flash_timer -= 1 / self.FPS
+
+        # Convert the canvas to a NumPy array
+        surface = pygame.surfarray.array3d(canvas)
+
+        return np.transpose(surface, axes=(1, 0, 2))
+
+
+    def _render_frame_human(self):
+        """
+        Render the environment in human mode.
+        """
+        if self.screen is None:  # Initialize Pygame if not already initialized
+            self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+            pygame.display.set_caption("Aerorequest Environment")
+            self.FramePerSec = pygame.time.Clock()
+
+        # Display background
+        self.screen.fill((131, 176, 181))
+
+        # Render clouds
+        self.cloud1_pos[0] += self.cloud1_speed
+        if self.cloud1_pos[0] > self.WIDTH:
+            self.cloud1_pos[0] = -self.cloud_images[0].get_width()
+        self.screen.blit(self.cloud_images[0], self.cloud1_pos)
+
+        self.cloud2_pos[0] += self.cloud2_speed
+        if self.cloud2_pos[0] < -self.cloud_images[1].get_width():
+            self.cloud2_pos[0] = self.WIDTH
+        self.screen.blit(self.cloud_images[1], self.cloud2_pos)
+
+        # Render sun
+        self.screen.blit(self.sun_image, self.sun_pos)
+
+        # Render player
+        player_copy = pygame.transform.rotate(self.player_image, self.player.angle)
+        player_pos = (
+            self.player.x_position - int(player_copy.get_width() / 2),
+            self.player.y_position - int(player_copy.get_height() / 2)
+        )
+        self.screen.blit(player_copy, player_pos)
+
+        # Render target
+        target_pos = (
+            self.targets[0][0] - int(self.target_image.get_width() / 2),
+            self.targets[0][1] - int(self.target_image.get_height() / 2)
+        )
+        self.screen.blit(self.target_image, target_pos)
+
+        # Check if flashing effect is active
+        if self.flash_timer > 0:
+            # Render the edges with flashing red color
+            pygame.draw.rect(self.screen, self.flash_color, (0, 0, self.WIDTH, 10))  # Top edge
+            pygame.draw.rect(self.screen, self.flash_color, (0, 0, 10, self.HEIGHT))  # Left edge
+            pygame.draw.rect(self.screen, self.flash_color, (0, self.HEIGHT - 10, self.WIDTH, 10))  # Bottom edge
+            pygame.draw.rect(self.screen, self.flash_color, (self.WIDTH - 10, 0, 10, self.HEIGHT))  # Right edge
+            # Decrement the flashing timer
+            self.flash_timer -= 1 / self.FPS
+
+        # Render time and score on the left side
+        font = pygame.font.SysFont(None, 36)
+        time_text = font.render(f"Time: {int(self.time)}", True, (255, 255, 255))
+        score_text = font.render(f"Score: {self.score}", True, (255, 255, 255))
         self.screen.blit(time_text, (20, 20))
         self.screen.blit(score_text, (20, 60))
 
         pygame.display.update()
         self.FramePerSec.tick(self.FPS)
 
-
-    def _render_rgb_array(self):
+    def _get_obs(self):
         """
-        Render the environment as an RGB array.
+        Get the observation of the environment.
 
         Returns:
-            np.array: Rendered image as an RGB array.
+            np.array or dict: Observation of the environment based on the observation type.
         """
+        if self.obs_type == 'pixels':
+            # Render the environment to get the observation
+            observation = self._render_frame()
+            # Convert the observation to a surface
+            observation_surface = pygame.surfarray.make_surface(observation)
+            # Resize the surface
+            observation_resized = pygame.transform.scale(observation_surface, (64, 64))
+            # Convert the resized surface back to a numpy array
+            observation_resized_np = pygame.surfarray.array3d(observation_resized)
+            return observation_resized_np
+        elif self.obs_type == 'features':
+            # Construct observation dictionary
+            observation = {
+                "player_position": np.array([self.player.x_position, self.player.y_position], dtype=np.float32),
+                "player_angle": np.array([self.player.angle], dtype=np.float32),
+                "target_position": np.array(self.targets[0], dtype=np.float32),
+                "score": np.array([self.score], dtype=np.float32)
+            }
+            return observation
 
-        surface = pygame.surfarray.array3d(self.screen)
-        return surface
-
-    def reset(self):
+    def reset(self, seed=None):
         """
         Reset the environment.
 
@@ -308,37 +448,10 @@ class Aerorequest(gym.Env):
         observation = self._get_obs()
         info = self._get_info()
 
-        if self.render_mode == "human":
-            self.render()
+        if self.render_mode == 'human':
+            self._render_frame_human()
 
         return observation, info
-
-    def _get_obs(self):
-        """
-        Get the observation of the environment.
-
-        Returns:
-            np.array or dict: Observation of the environment based on the observation type.
-        """
-        if self.obs_type == 'pixels':
-            # Render the environment to get the observation
-            observation = self._render_rgb_array()
-            # Convert the observation to a surface
-            observation_surface = pygame.surfarray.make_surface(observation)
-            # Resize the surface
-            observation_resized = pygame.transform.scale(observation_surface, (64, 64))
-            # Convert the resized surface back to a numpy array
-            observation_resized_np = pygame.surfarray.array3d(observation_resized)
-            return observation_resized_np
-        elif self.obs_type == 'features':
-            # Construct observation dictionary
-            observation = {
-                "player_position": np.array([self.player.x_position, self.player.y_position], dtype=np.float32),
-                "player_angle": np.array([self.player.angle], dtype=np.float32),
-                "target_position": np.array(self.targets[0], dtype=np.float32),
-                "score": np.array([self.score], dtype=np.float32)
-            }
-            return observation
 
     def _get_info(self):
         """
@@ -357,4 +470,3 @@ class Aerorequest(gym.Env):
         Close the Pygame window.
         """
         pygame.quit()
-
